ba38aa2edc1c5a430a58f0d54eadee11cc87965e
abyss <a00411@naver.com>
2025-02-10 19:35:27 +0900

    commit message

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{logo }}
+    <h4 class="red" :style="스타일">xx 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>xx 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name : 'App',
+  data() {
+    return{
+      product :[ '역사동원룸', '천호동원룸','마포구원룸'],
+    }
+  },
+  components : {
+  }
+}
+</script>
+
+<style>
+
+</style> 
