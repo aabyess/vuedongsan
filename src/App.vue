94dd4c5e57767df8e2016cbe2d61c888ff9e2e9a
abyss <a00411@naver.com>
2025-02-11 17:59:09 +0900

    0211
    0211공부_1

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <template>
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+  </div>
   <div>
-    <h4>{{ product[0] }}</h4>
+    <h4>{{ products[0] }}</h4>
     <p>50 만원</p>
+    <button @click="신고수[0]++">허위매물신고</button><span>신고수 : {{ 신고수[0]}}</span>
   </div>
   <div>
-    <h4>{{ product[1] }}</h4>
+    <h4>{{ products[1] }}</h4>
     <p>가격은 아무거나</p>
+    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
   </div>
   <div>
-    <h4>{{ product[2] }}</h4>
+    <h4>{{ products[2] }}</h4>
     <p>가격은 아무거나</p>
+    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
   </div>
 </template>
 
 <script>
+function 어쩌구(){
+  console.log("어쩌구입니다.");
+  console.log("어쩌구입니다.");
+  console.log("어쩌구입니다.");
+}
+
 
 export default {
   name : 'App',
   data() {
     return{
-      product :[ '역사동원룸', '천호동원룸','마포구원룸'],
+      신고수 : [0,0,0],
+      메뉴들 : ['Home', 'Shop','About'],
+      products : [ '역사동원룸', '천호동원룸','마포구원룸'],
+    }
+  },
+  methods:{
+    increase(){
+      this.신고수++;
     }
   },
   components : {
@@ -28,5 +47,13 @@ export default {
 </script>
 
 <style>
-
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding :10px;
+}
 </style> 
